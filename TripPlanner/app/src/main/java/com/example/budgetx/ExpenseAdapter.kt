2478d1e5c3cb9fb4e66ef21a56777c85f6090612
Thaps171 <tekhoza17@gmail.com>
2026-09-21 21:18:28 +0200

    yuh

--- TripPlanner/app/src/main/java/com/example/budgetx/ExpenseAdapter.kt
+++ TripPlanner/app/src/main/java/com/example/budgetx/ExpenseAdapter.kt
@@ -8,7 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.budgetx.data.entity.Expense
 import com.example.budgetx.databinding.ItemExpenseBinding
 import java.io.File
-
+// This code 
 class ExpenseAdapter(private var expenses: List<Expense>) : RecyclerView.Adapter<ExpenseAdapter.ExpenseViewHolder>() {
     class ExpenseViewHolder(val binding: ItemExpenseBinding):RecyclerView.ViewHolder(binding.root)
     override fun onCreateViewHolder(parent:ViewGroup,viewType:Int)=ExpenseViewHolder(ItemExpenseBinding.inflate(LayoutInflater.from(parent.context),parent,false))
