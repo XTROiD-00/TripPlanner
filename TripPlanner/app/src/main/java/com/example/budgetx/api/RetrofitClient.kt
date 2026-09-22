cc7c4c873e030dc9c6b6d582ecbdd3f958052963
Thaps171 <tekhoza17@gmail.com>
2026-09-22 11:46:25 +0200

    api

--- TripPlanner/app/src/main/java/com/example/budgetx/api/RetrofitClient.kt
+++ TripPlanner/app/src/main/java/com/example/budgetx/api/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "https://tripplanner-9wey.onrender.com/"
 
     val api: TripApiService by lazy {
         Retrofit.Builder()
